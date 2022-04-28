933517b943e3bf3eab9fb68e37527f174c84289e
Wu Wenhan <1020645987@qq.com>
2022-04-28 17:36:29 +0800

    merge index-stellar into assets/js

--- assets/js/first-page.js
+++ assets/js/first-page.js
@@ -10,22 +10,22 @@
 		$body = $('body'),
 		$main = $('#main');
 
-	// Breakpoints.
-	breakpoints({
-		xlarge: ['1281px', '1680px'],
-		large: ['981px', '1280px'],
-		medium: ['737px', '980px'],
-		small: ['481px', '736px'],
-		xsmall: ['361px', '480px'],
-		xxsmall: [null, '360px']
-	});
+	//Breakpoints.
+	// breakpoints({
+	// 	xlarge: ['1281px', '1680px'],
+	// 	large: ['981px', '1280px'],
+	// 	medium: ['737px', '980px'],
+	// 	small: ['481px', '736px'],
+	// 	xsmall: ['361px', '480px'],
+	// 	xxsmall: [null, '360px']
+	// });
 
 	// Play initial animations on page load.
-	$window.on('load', function () {
-		window.setTimeout(function () {
-			$body.removeClass('is-preload');
-		}, 100);
-	});
+	// $window.on('load', function () {
+	// 	window.setTimeout(function () {
+	// 		$body.removeClass('is-preload');
+	// 	}, 100);
+	// });
 
 	// Nav.
 
@@ -98,8 +98,8 @@
 				initialize: function () {
 
 					// Deactivate section.
-					if (browser.canUse('transition'))
-						$section.addClass('inactive');
+					// if (browser.canUse('transition'))
+					// 	$section.addClass('inactive');
 
 				},
 				enter: function () {
